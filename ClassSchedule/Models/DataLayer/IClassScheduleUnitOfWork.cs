6a451c065b24e9e3c3bd780bf605a212908a5d04
Worod Zik <worodzik@yahoo.com>
2022-05-07 00:33:50 -0400

    Add project files.

---
+++ ClassSchedule/Models/DataLayer/IClassScheduleUnitOfWork.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClassSchedule.Models
+{
+    interface IClassScheduleUnitOfWork
+    {
+        public Class Class { get; }
+
+        public Teacher Teacher { get; }
+        public Day Day { get; }
+
+
+
+
+
+        
+       
+    }
+}
